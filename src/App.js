9d77fe8b2fa0a6d18c107c63fe3333ee7293e125
chaijewon <vcandjava@nate.com>
2020-12-02 12:01:02 +0900

    1111

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{Component} from 'react'
+import axios from 'axios'
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
+import ChefList from "./ChefList";
+import ChefDetail from "./ChefDetail";
+// model.addAttribute("main_jsp","파일명")
+// React => 한개의 기능수행
+class App extends Component{
+   render(){
+      return (
+          <Router>
+            <Switch>
+              <Route exact path={"/"} component={ChefList}/>
+              <Route exact path={"/chef_detail"} component={ChefDetail}/>
+            </Switch>
+          </Router>
+      )
+   }
 }
-
 export default App;
